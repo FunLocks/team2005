7b5f656d1aa99fe5da89a77e1d2f32f2272b3009
kota-shiokara <ikanoshiokara.fun@gmail.com>
2020-12-17 04:24:58 +0900

    gradleの更新

--- Avoavo/app/src/main/java/com/yeahoohunters/avoavo/model/api/responce/NowSituation.kt
+++ Avoavo/app/src/main/java/com/yeahoohunters/avoavo/model/api/responce/NowSituation.kt
@@ -1,9 +1,12 @@
 package com.yeahoohunters.avoavo.model.api.responce
 
-
+import androidx.room.Entity
 
 data class NowSituation (
-    val congestion: List<Int>
+    val congestion: Congestion
 ){
-
+    @Entity(tableName = "now_situation")
+    data class Congestion(
+        val congestion: ArrayList<Int>
+    )
 }
