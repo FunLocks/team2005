381da05fada0a6e5ca307565b535174e55a18d09
sora <kacky1210@gmail.com>
2020-12-16 22:47:11 +0900

    送るjsonの形を修正

--- back/now_people.py
+++ back/now_people.py
@@ -5,7 +5,12 @@ def now_congection():
     dt_now = datetime.datetime.now()
     dt_5m_ago = str(dt_now - datetime.timedelta(minutes=5))[:19]
     t = f"select location_id, sum(count) as congection from (select location_id, count(*) from beacon_data group by location_id, time having time between '{dt_5m_ago}' and '{str(dt_now)[:19]}' order by location_id asc) as counts group by location_id"
-    congection_dict = {}
+    location_id = {}
+    congection = {}
+    for i in range(77):
+        location_id[str(i)] = i + 1
+        congection[str(i)] = 0
     for r in db.session.execute(t):
-        congection_dict[r['location_id']] = r['congection']
-    return congection_dict
+        congection[str(r['location_id']-1)] = r['congection']
+    now_data = {"location_id": location_id, "congection": congection}
+    return now_data
