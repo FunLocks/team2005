e00075e4d47ada68ab503fca770ccd4faac37c07
加藤まる <rikka2217@gmail.com>
2020-12-16 19:09:58 +0900

    混雑予測データを作った

--- back/GenData.py
+++ back/GenData.py
@@ -16,21 +16,42 @@ class GenerateData:
         return
 
     def favorite(self, random_min: int, random_max: int):
-        fav: DataFrame = pd.DataFrame(columns=['congection'])
-        
-        for i in range(1,25):
-            fav.loc[i] = (random.randint(random_min,random_max))
-
+        fav: DataFrame = pd.DataFrame(columns=['location_id','time','congection'])
         return fav
+    
+    def predict(self,random_min: int,random_max: int):
+        floor_1 = [1,2,3,4,5,60,61,62]
+        floor_2 = [6,7,8,9,10,63,64,65]
+        floor_3 = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,66,67,68,69,70]
+        floor_4 = [25,26,27,28,29,30,31,32,33,34,35,36,71,72,73,74]
+        floor_5 = [37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59]
+        time = [-1,0,1,2,3,4,5,6,7,8,9,10,11,12]
+
+        def make_data(floor: str,floorlist: list):
+            df_1 = pd.DataFrame(columns=['location_id','time','congection'])
+            for i in floorlist:
+                for j in (time):
+                    df_1 = df_1.append({'location_id':i, 'time':j,'congection': random.randint(random_min,random_max)}, ignore_index=True)
+
+            print(df_1)
+            json_data = df_1.to_dict()
+
+            filename = "sampledata/predict/" + floor + ".json"
+            print(filename)
+            with open(filename, 'w') as f:
+                json.dump(json_data, f, ensure_ascii=False)
+            
+            return
+            
+        make_data("floor_1",floor_1)
+        make_data("floor_2",floor_2)
+        make_data("floor_3",floor_3)
+        make_data("floor_4",floor_4)
+        make_data("floor_5",floor_5)
+
+        return
 
 
 if __name__ == '__main__':
     generator = GenerateData()
-    for i in range(1,78):
-        sample_data = generator.favorite(1,25)
-        json_data = sample_data.to_dict()
-
-        filename = "sampledata/favorite/location_id_" + str(i) + ".json"
-        print(filename)
-        with open(filename, 'w') as f:
-            json.dump(json_data, f, ensure_ascii=False)
+    generator.predict(1,20)
