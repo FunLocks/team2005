91a0e898ff9681b9b4bc93981e394f1e19c18848
加藤まる <rikka2217@gmail.com>
2020-12-17 22:13:13 +0900

    お気に入りのrootつくった

--- back/app.py
+++ back/app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template,jsonify,request # 変更
 from database import db, beacon_data
 from now_people import now_congection
+from favorite_predict import favorite_predict
+import json
+
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 app.config.from_object('config.Config')
 db.init_app(app)
 
 @app.route('/now_situation', methods=['GET'])
-def get():
+def get_now():
     response = now_congection()
     return jsonify(response)
+
+@app.route('/favorite_predict/<location_id>', methods=['GET'])
+def get_fav(location_id):
+    #response = favorite_predict(location_id)
+    #return jsonify(response)
+
+    json_open = open('sampledata/favorite/location_id_'+location_id+'.json', 'r')
+    json_load = json.load(json_open)
+    return jsonify(json_load)
 
 
 @app.route('/post', methods=['POST'])
