14745c3d111af8e17258d5592469cfb4c6290436
sora <kacky1210@gmail.com>
2020-12-19 06:10:41 +0900

    homeフラグメントの記述を終えた

--- Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/HomeFourthFragment.kt
+++ Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/HomeFourthFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yeahoohunters.avoavo.R
+import android.graphics.PorterDuff
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_home_fourth. *
 
 class HomeFourthFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,306 @@ class HomeFourthFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_fourth, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val red = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.red) }?.let { red.setTint(it) }
+        red.setTintMode(PorterDuff.Mode.SRC_IN)
+        val orange = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.orange) }?.let { orange.setTint(it) }
+        orange.setTintMode(PorterDuff.Mode.SRC_IN)
+        val green = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.green) }?.let { green.setTint(it) }
+        green.setTintMode(PorterDuff.Mode.SRC_IN)
+
+        var fourth: MutableMap<Int, Int> = mutableMapOf()
+
+        for (i in 25..36) {
+            arguments?.getInt(i.toString())?.let { fourth.put(i, it) }
+        }
+        for (i in 71..74) {
+            arguments?.getInt(i.toString())?.let { fourth.put(i, it) }
+        }
+
+        for ((k,v) in fourth){
+            when(k){
+                25 -> {
+                    when(v){
+                        0 -> if (circle_25.visibility == View.VISIBLE) circle_25.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_25.visibility == View.INVISIBLE) circle_25.visibility = View.VISIBLE
+                            circle_25.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_25.visibility == View.INVISIBLE) circle_25.visibility = View.VISIBLE
+                            circle_25.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_25.visibility == View.INVISIBLE) circle_25.visibility = View.VISIBLE
+                            circle_25.setImageDrawable(red)
+                        }
+                    }
+                }
+                26 -> {
+                    when(v){
+                        0 -> if (circle_26.visibility == View.VISIBLE) circle_26.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_26.visibility == View.INVISIBLE) circle_26.visibility = View.VISIBLE
+                            circle_26.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_26.visibility == View.INVISIBLE) circle_26.visibility = View.VISIBLE
+                            circle_26.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_26.visibility == View.INVISIBLE) circle_26.visibility = View.VISIBLE
+                            circle_26.setImageDrawable(red)
+                        }
+                    }
+                }
+                27 -> {
+                    when(v){
+                        0 -> if (circle_27.visibility == View.VISIBLE) circle_27.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_27.visibility == View.INVISIBLE) circle_27.visibility = View.VISIBLE
+                            circle_27.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_27.visibility == View.INVISIBLE) circle_27.visibility = View.VISIBLE
+                            circle_27.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_27.visibility == View.INVISIBLE) circle_27.visibility = View.VISIBLE
+                            circle_27.setImageDrawable(red)
+                        }
+                    }
+                }
+                28 -> {
+                    when(v){
+                        0 -> if (circle_28.visibility == View.VISIBLE) circle_28.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_28.visibility == View.INVISIBLE) circle_28.visibility = View.VISIBLE
+                            circle_28.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_28.visibility == View.INVISIBLE) circle_28.visibility = View.VISIBLE
+                            circle_28.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_28.visibility == View.INVISIBLE) circle_28.visibility = View.VISIBLE
+                            circle_28.setImageDrawable(red)
+                        }
+                    }
+                }
+                29 -> {
+                    when(v){
+                        0 -> if (circle_29.visibility == View.VISIBLE) circle_29.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_29.visibility == View.INVISIBLE) circle_29.visibility = View.VISIBLE
+                            circle_29.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_29.visibility == View.INVISIBLE) circle_29.visibility = View.VISIBLE
+                            circle_29.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_29.visibility == View.INVISIBLE) circle_29.visibility = View.VISIBLE
+                            circle_29.setImageDrawable(red)
+                        }
+                    }
+                }
+                30 -> {
+                    when(v){
+                        0 -> if (circle_30.visibility == View.VISIBLE) circle_30.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_30.visibility == View.INVISIBLE) circle_30.visibility = View.VISIBLE
+                            circle_30.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_30.visibility == View.INVISIBLE) circle_30.visibility = View.VISIBLE
+                            circle_30.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_30.visibility == View.INVISIBLE) circle_30.visibility = View.VISIBLE
+                            circle_30.setImageDrawable(red)
+                        }
+                    }
+                }
+                31 -> {
+                    when(v){
+                        0 -> if (circle_31.visibility == View.VISIBLE) circle_31.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_31.visibility == View.INVISIBLE) circle_31.visibility = View.VISIBLE
+                            circle_31.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_31.visibility == View.INVISIBLE) circle_31.visibility = View.VISIBLE
+                            circle_31.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_31.visibility == View.INVISIBLE) circle_31.visibility = View.VISIBLE
+                            circle_31.setImageDrawable(red)
+                        }
+                    }
+                }
+                32 -> {
+                    when(v){
+                        0 -> if (circle_32.visibility == View.VISIBLE) circle_32.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_32.visibility == View.INVISIBLE) circle_32.visibility = View.VISIBLE
+                            circle_32.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_32.visibility == View.INVISIBLE) circle_32.visibility = View.VISIBLE
+                            circle_32.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_32.visibility == View.INVISIBLE) circle_32.visibility = View.VISIBLE
+                            circle_32.setImageDrawable(red)
+                        }
+                    }
+                }
+                33 -> {
+                    when(v){
+                        0 -> if (circle_33.visibility == View.VISIBLE) circle_33.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_33.visibility == View.INVISIBLE) circle_33.visibility = View.VISIBLE
+                            circle_33.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_33.visibility == View.INVISIBLE) circle_33.visibility = View.VISIBLE
+                            circle_33.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_33.visibility == View.INVISIBLE) circle_33.visibility = View.VISIBLE
+                            circle_33.setImageDrawable(red)
+                        }
+                    }
+                }
+                34 -> {
+                    when(v){
+                        0 -> if (circle_34.visibility == View.VISIBLE) circle_34.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_34.visibility == View.INVISIBLE) circle_34.visibility = View.VISIBLE
+                            circle_34.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_34.visibility == View.INVISIBLE) circle_34.visibility = View.VISIBLE
+                            circle_34.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_34.visibility == View.INVISIBLE) circle_34.visibility = View.VISIBLE
+                            circle_34.setImageDrawable(red)
+                        }
+                    }
+                }
+                35 -> {
+                    when(v){
+                        0 -> if (circle_35.visibility == View.VISIBLE) circle_35.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_35.visibility == View.INVISIBLE) circle_35.visibility = View.VISIBLE
+                            circle_35.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_35.visibility == View.INVISIBLE) circle_35.visibility = View.VISIBLE
+                            circle_35.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_35.visibility == View.INVISIBLE) circle_35.visibility = View.VISIBLE
+                            circle_35.setImageDrawable(red)
+                        }
+                    }
+                }
+                36 -> {
+                    when(v){
+                        0 -> if (circle_36.visibility == View.VISIBLE) circle_36.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_36.visibility == View.INVISIBLE) circle_36.visibility = View.VISIBLE
+                            circle_36.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_36.visibility == View.INVISIBLE) circle_36.visibility = View.VISIBLE
+                            circle_36.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_36.visibility == View.INVISIBLE) circle_36.visibility = View.VISIBLE
+                            circle_36.setImageDrawable(red)
+                        }
+                    }
+                }
+                71 -> {
+                    when(v){
+                        0 -> if (circle_71.visibility == View.VISIBLE) circle_71.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_71.visibility == View.INVISIBLE) circle_71.visibility = View.VISIBLE
+                            circle_71.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_71.visibility == View.INVISIBLE) circle_71.visibility = View.VISIBLE
+                            circle_71.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_71.visibility == View.INVISIBLE) circle_71.visibility = View.VISIBLE
+                            circle_71.setImageDrawable(red)
+                        }
+                    }
+                }
+                72 -> {
+                    when(v){
+                        0 -> if (circle_72.visibility == View.VISIBLE) circle_72.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_72.visibility == View.INVISIBLE) circle_72.visibility = View.VISIBLE
+                            circle_72.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_72.visibility == View.INVISIBLE) circle_72.visibility = View.VISIBLE
+                            circle_72.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_72.visibility == View.INVISIBLE) circle_72.visibility = View.VISIBLE
+                            circle_72.setImageDrawable(red)
+                        }
+                    }
+                }
+                73 -> {
+                    when(v){
+                        0 -> if (circle_73.visibility == View.VISIBLE) circle_73.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_73.visibility == View.INVISIBLE) circle_73.visibility = View.VISIBLE
+                            circle_73.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_73.visibility == View.INVISIBLE) circle_73.visibility = View.VISIBLE
+                            circle_73.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_73.visibility == View.INVISIBLE) circle_73.visibility = View.VISIBLE
+                            circle_73.setImageDrawable(red)
+                        }
+                    }
+                }
+                74 -> {
+                    when(v){
+                        0 -> if (circle_74.visibility == View.VISIBLE) circle_74.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_74.visibility == View.INVISIBLE) circle_74.visibility = View.VISIBLE
+                            circle_74.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_74.visibility == View.INVISIBLE) circle_74.visibility = View.VISIBLE
+                            circle_74.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_74.visibility == View.INVISIBLE) circle_74.visibility = View.VISIBLE
+                            circle_74.setImageDrawable(red)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/HomeSecondFragment.kt
+++ Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/HomeSecondFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yeahoohunters.avoavo.R
+import android.graphics.PorterDuff
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_home_second.*
 
 class HomeSecondFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +19,170 @@ class HomeSecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val red = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.red) }?.let { red.setTint(it) }
+        red.setTintMode(PorterDuff.Mode.SRC_IN)
+        val orange = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.orange) }?.let { orange.setTint(it) }
+        orange.setTintMode(PorterDuff.Mode.SRC_IN)
+        val green = resources.getDrawable(R.drawable.congestion_circle, null)
+        activity?.applicationContext?.let { ContextCompat.getColor(it, R.color.green) }?.let { green.setTint(it) }
+        green.setTintMode(PorterDuff.Mode.SRC_IN)
+
+        var second: MutableMap<Int, Int> = mutableMapOf()
+
+        for (i in 6..10) {
+            arguments?.getInt(i.toString())?.let { second.put(i, it) }
+        }
+        for (i in 63..65) {
+            arguments?.getInt(i.toString())?.let { second.put(i, it) }
+        }
+
+        for ((k,v) in second){
+            when(k){
+                6 -> {
+                    when(v){
+                        0 -> if (circle_6.visibility == View.VISIBLE) circle_6.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_6.visibility == View.INVISIBLE) circle_6.visibility = View.VISIBLE
+                            circle_6.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_6.visibility == View.INVISIBLE) circle_6.visibility = View.VISIBLE
+                            circle_6.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_6.visibility == View.INVISIBLE) circle_6.visibility = View.VISIBLE
+                            circle_6.setImageDrawable(red)
+                        }
+                    }
+                }
+                7 -> {
+                    when(v){
+                        0 -> if (circle_7.visibility == View.VISIBLE) circle_7.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_7.visibility == View.INVISIBLE) circle_7.visibility = View.VISIBLE
+                            circle_7.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_7.visibility == View.INVISIBLE) circle_7.visibility = View.VISIBLE
+                            circle_7.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_7.visibility == View.INVISIBLE) circle_7.visibility = View.VISIBLE
+                            circle_7.setImageDrawable(red)
+                        }
+                    }
+                }
+                8 -> {
+                    when(v){
+                        0 -> if (circle_8.visibility == View.VISIBLE) circle_8.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_8.visibility == View.INVISIBLE) circle_8.visibility = View.VISIBLE
+                            circle_8.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_8.visibility == View.INVISIBLE) circle_8.visibility = View.VISIBLE
+                            circle_8.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_8.visibility == View.INVISIBLE) circle_8.visibility = View.VISIBLE
+                            circle_8.setImageDrawable(red)
+                        }
+                    }
+                }
+                9 -> {
+                    when(v){
+                        0 -> if (circle_9.visibility == View.VISIBLE) circle_9.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_9.visibility == View.INVISIBLE) circle_9.visibility = View.VISIBLE
+                            circle_9.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_9.visibility == View.INVISIBLE) circle_9.visibility = View.VISIBLE
+                            circle_9.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_9.visibility == View.INVISIBLE) circle_9.visibility = View.VISIBLE
+                            circle_9.setImageDrawable(red)
+                        }
+                    }
+                }
+                10 -> {
+                    when(v){
+                        0 -> if (circle_10.visibility == View.VISIBLE) circle_10.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_10.visibility == View.INVISIBLE) circle_10.visibility = View.VISIBLE
+                            circle_10.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_10.visibility == View.INVISIBLE) circle_10.visibility = View.VISIBLE
+                            circle_10.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_10.visibility == View.INVISIBLE) circle_10.visibility = View.VISIBLE
+                            circle_10.setImageDrawable(red)
+                        }
+                    }
+                }
+                63 -> {
+                    when(v){
+                        0 -> if (circle_63.visibility == View.VISIBLE) circle_63.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_63.visibility == View.INVISIBLE) circle_63.visibility = View.VISIBLE
+                            circle_63.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_63.visibility == View.INVISIBLE) circle_63.visibility = View.VISIBLE
+                            circle_63.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_63.visibility == View.INVISIBLE) circle_63.visibility = View.VISIBLE
+                            circle_63.setImageDrawable(red)
+                        }
+                    }
+                }
+                64 -> {
+                    when(v){
+                        0 -> if (circle_64.visibility == View.VISIBLE) circle_64.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_64.visibility == View.INVISIBLE) circle_64.visibility = View.VISIBLE
+                            circle_64.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_64.visibility == View.INVISIBLE) circle_64.visibility = View.VISIBLE
+                            circle_64.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_64.visibility == View.INVISIBLE) circle_64.visibility = View.VISIBLE
+                            circle_64.setImageDrawable(red)
+                        }
+                    }
+                }
+                65 -> {
+                    when(v){
+                        0 -> if (circle_65.visibility == View.VISIBLE) circle_65.visibility = View.INVISIBLE
+                        in 1..5 -> {
+                            if (circle_65.visibility == View.INVISIBLE) circle_65.visibility = View.VISIBLE
+                            circle_65.setImageDrawable(green)
+                        }
+                        in 6..10 -> {
+                            if (circle_65.visibility == View.INVISIBLE) circle_65.visibility = View.VISIBLE
+                            circle_65.setImageDrawable(orange)
+                        }
+                        in 11..15 -> {
+                            if (circle_65.visibility == View.INVISIBLE) circle_65.visibility = View.VISIBLE
+                            circle_65.setImageDrawable(red)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
