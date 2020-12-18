43ddcc24065f00209e4e70dbcf400abb4003bd43
kota-shiokara <ikanoshiokara.fun@gmail.com>
2020-12-19 07:35:49 +0900

    toolbarの色を調整

--- Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/MainActivity.kt
+++ Avoavo/app/src/main/java/com/yeahoohunters/avoavo/view/MainActivity.kt
@@ -9,6 +9,8 @@ import android.annotation.SuppressLint
 import android.os.RemoteException
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.fragment_forecast.view.*
 import org.altbeacon.beacon.*
 import permissions.dispatcher.*
 import java.sql.Date
@@ -76,7 +78,9 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
             .replace(R.id.frameLayout, HomeFragment())
             .commit()
 
-
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_fun_locks_icon)
+        setSupportActionBar(toolbar)
 
         beaconManager = BeaconManager.getInstanceForApplication(this)
         beaconManager.isRegionStatePersistenceEnabled = false
